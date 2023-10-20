66d497c1c8d7e18c25cd36f2b627c4b897c3bcd2
rnddfn <130640124+rnddfn@users.noreply.github.com>
2023-10-20 09:29:31 +0800

    PRAK201-2310817110013-Randy_Febrian.c

--- Modul-2/Soal-1/PRAK201-2310817110013-Randy_Febrian.c
+++ Modul-2/Soal-1/PRAK201-2310817110013-Randy_Febrian.c
@@ -3,7 +3,6 @@
 int main(){
     char nama[30]; char NIM[30]; char paralel[30]; char TTL[30]; char alamat[50]; char hobby[30]; char no_hp[30];
 
-    //INPUT
     printf("INPUT\n");
     printf("Nama                : ");
     fgets(nama, sizeof(nama), stdin);
@@ -21,7 +20,6 @@ int main(){
     fgets(no_hp, sizeof(no_hp), stdin);
     printf("\n");
 
-    //OUTPUT
     printf("OUTPUT\n");
     printf("Nama                : %s", nama);
     printf("NIM                 : %s", NIM);
